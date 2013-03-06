8109f72c1034e6f01ca37e7ed4b027efd9333c9a
Chris Emerson <github@mail.nosreme.org>
2013-03-06 21:52:21 +0000

    Implement number prefixes to some commands.

--- vi_mode.lua
+++ vi_mode.lua
@@ -10,7 +10,7 @@ INSERT = "insert"
 mode = nil  -- initialised below
 local key_handler = nil
 
-local debug = false
+local debug = true
 
 function dbg(...)
     --if debug then print(...) end
@@ -63,9 +63,38 @@ mode_insert = {
     end,
 
     bindings = {
-        esc = function() enter_mode(mode_command) end,
+        esc = function() 
+	    local line, pos = buffer.get_cur_line()
+            if pos > 0 then buffer.char_left() end
+            enter_mode(mode_command)
+        end,
     }
 }
+
+command_numarg = 0
+function addarg(n)
+  command_numarg = command_numarg * 10 + n
+  dbg("New numarg: " .. tostring(command_numarg))
+end
+-- Return a handler for digit n
+function dodigit(n)
+    return function() addarg(n) end
+end
+
+-- Wrapper to turn a simple command into one which uses the numeric prefix
+local function repeat_arg(f)
+  -- Run f command_numarg times (if command_numarg is non-zero) and clear
+  -- command_numarg
+  return function()
+     local times = command_numarg
+     command_numarg = 0
+     if times == 0 then times = 1 end
+     for i=1,times do
+         f()
+     end
+  end
+end
+
 mode_command = {
     name = COMMAND,
 
@@ -75,21 +104,53 @@ mode_command = {
 
     bindings = {
         -- movement commands
-        h = function ()
+        h = repeat_arg(function ()
 	  local line, pos = buffer.get_cur_line()
 	  if pos > 0 then buffer.char_left() end
-        end,
-        l = function()
+        end),
+        l = repeat_arg(function()
 	  local line, pos = buffer.get_cur_line()
 	  local docpos = buffer.current_pos
 	  local length = buffer.line_length(buffer.line_from_position(docpos))
 	  if pos < (length - 1) then
 	      buffer.char_right()
 	  end
-        end,
-        j = buffer.line_down,
-        k = buffer.line_up,
-	['0'] = buffer.home,
+        end),
+        j = repeat_arg(buffer.line_down),
+        k = repeat_arg(buffer.line_up),
+        H = function()
+             -- We can't use goto_line here as it scrolls the window slightly.
+             local top_line = buffer.first_visible_line
+             local pos = buffer.position_from_line(top_line)
+             buffer.current_pos = pos
+             buffer.anchor = pos
+            end,
+        M = function()
+             buffer.goto_line(buffer.first_visible_line + buffer.lines_on_screen/2)
+            end,
+        L = function()
+             local bot_line = buffer.first_visible_line + buffer.lines_on_screen - 1
+             local pos = buffer.position_from_line(bot_line)
+             buffer.current_pos = pos
+             buffer.anchor = pos
+            end,
+
+	['0'] = function()
+             if command_numarg == 0 then
+                buffer.home()
+             else
+                addarg(0)
+	     end
+          end,
+	['1'] = dodigit(1),
+	['2'] = dodigit(2),
+	['3'] = dodigit(3),
+	['4'] = dodigit(4),
+	['5'] = dodigit(5),
+	['6'] = dodigit(6),
+	['7'] = dodigit(7),
+	['8'] = dodigit(8),
+	['9'] = dodigit(9),
 	['$'] = function()
 		 -- Stop just before the end
 		 buffer.line_end()
@@ -98,16 +159,24 @@ mode_command = {
 	       end,
 	['^'] = function()
 		   buffer.home()    -- Go to beginning of line
-                   buffer.vc_home()  -- swaps between beginning/first visible
+		   buffer.vc_home()  -- swaps between beginning/first visible
                 end,
 	G = function()
+	       if command_numarg > 0 then
+                   -- Textadept does zero-based line numbers.
+		   buffer.goto_line(command_numarg - 1)
+		   command_numarg = 0
+	       else    
+                 -- With no arg, go to last line.
                  buffer.document_end()
 		 buffer.home()
-               end,
+	       end
+	   end,
 
 	-- edit mode commands
         i = function() enter_mode(mode_insert) end,
 	a = function() buffer.char_right() enter_mode(mode_insert) end,
+        A = function() buffer.line_end() enter_mode(mode_insert) end,
         
         -- edit commands
 	u = buffer.undo,
