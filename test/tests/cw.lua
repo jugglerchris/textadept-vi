f4b175676cee86b2add90ede65df74f4da98ffcb
Chris Emerson <github@mail.nosreme.org>
2013-11-06 21:57:07 +0000

    Add more test cases with counts (now fails).

--- test/tests/cw.lua
+++ test/tests/cw.lua
@@ -18,3 +18,19 @@ assertEq(buffer:get_cur_line(), "foo bee foo dee ee eff\n")
 -- undo and check it's been reverted
 test.key('u', 'u')
 assertEq(buffer:get_cur_line(), "hey bee cee dee ee eff\n")
+-- Try again with a prefix
+test.key('0') -- ensure start of line
+test.key('3', '.')
+-- Prefix should repeat the movement, not the change.
+assertEq(buffer:get_cur_line(), "foo dee ee eff\n")
+test.key('u')
+assertEq(buffer:get_cur_line(), "hey bee cee dee ee eff\n")
+-- Try existing count
+test.key('2', 'c', 'w', 'r', 's', 't', 'space', 'escape')
+assertEq(buffer:get_cur_line(), "rst cee dee ee eff\n")
+test.key('u')
+assertEq(buffer:get_cur_line(), "hey bee cee dee ee eff\n")
+-- Try replacing the count, should change the number of words changed.
+test.key('0')
+test.key('3', '.')
+assertEq(buffer:get_cur_line(), "rst dee ee eff\n")
