01a7202eb2d491580cdd9a82feb819a9ec65b7fa
Chris Emerson <github@mail.nosreme.org>
2013-11-18 20:20:08 +0000

    Add a test for J (join lines).

---
+++ test/tests/J.lua
@@ -0,0 +1,25 @@
+test.open('indents2.txt')
+local lineno = test.lineno
+local colno = test.colno
+local assertEq = test.assertEq
+local log = test.log
+
+assertEq(lineno(), 0) assertEq(colno(), 0)
+
+test.key('J')
+assertEq(colno(), 9)
+assertEq(buffer:get_line(0), 'blah blah woo woo\n')
+test.key('J')
+assertEq(colno(), 17)
+assertEq(buffer:get_line(0), 'blah blah woo woo erg  -- trailing spaces    \n')
+test.key('J')
+assertEq(colno(), 45)
+assertEq(buffer:get_line(0), 'blah blah woo woo erg  -- trailing spaces     sponge\n')
+
+test.key('j', 'j', '0')
+assertEq(lineno(), 2) assertEq(colno(), 0)
+assertEq(buffer:get_line(2), 'This is a sentence.\n')
+
+test.key('J')
+assertEq(lineno(), 2) assertEq(colno(), 19)
+assertEq(buffer:get_line(2), 'This is a sentence.  And another.\n')
