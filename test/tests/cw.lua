ee4e7b4d144b592baac15156c13a15c9a268f577
Chris Emerson <github@mail.nosreme.org>
2013-11-24 00:05:54 +0000

    Update cw test to better match vim behaviour - it's treated more like ce (doesn't include the space following the word).

--- test/tests/cw.lua
+++ test/tests/cw.lua
@@ -8,8 +8,8 @@ assertEq(colno(), 0)
 assertEq(lineno(), 1)
 assertEq(buffer:get_cur_line(), "hey bee cee dee ee eff\n")
 -- Test changing the first word
-test.key('c', 'w', 'f', 'o', 'o', 'space', 'escape')
-assertEq(colno(), 3)
+test.key('c', 'w', 'f', 'o', 'o', 'escape')
+assertEq(colno(), 2)
 assertEq(lineno(), 1)
 assertEq(buffer:get_cur_line(), "foo bee cee dee ee eff\n")
 -- Move forward a couple of words (to the cee), then repeat
