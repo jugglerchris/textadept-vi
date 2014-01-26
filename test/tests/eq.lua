d2fd990aada88beadc5b51e1e487601d9920e080
Chris Emerson <github@mail.nosreme.org>
2014-01-26 23:23:18 +0000

    Add a blank line to the XML reindent test.

--- test/tests/eq.lua
+++ test/tests/eq.lua
@@ -15,18 +15,18 @@ local function get_indents()
   end
   return indents
 end
-assertEq(get_indents(), {0, 2, 2, 4, 4, 4, 4, 4, 4, 2, 4, 2, 0})
+assertEq(get_indents(), {0, 2, 2, 4, 4, 4, 4, 0, 4, 4, 2, 4, 2, 0})
 test.key('u')
-assertEq(get_indents(), {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+assertEq(get_indents(), {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 test.keys('2G==')
-assertEq(get_indents(), {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+assertEq(get_indents(), {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 -- Regression test: this was returning ...,2,1,0.
 test.keys('u11G==12G==')
-assertEq(get_indents(), {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0})
+assertEq(get_indents(), {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0})
 
 --
 test.keys('uu1G=G') -- undo and re-indent all
-assertEq(get_indents(), {0, 2, 2, 4, 4, 4, 4, 4, 4, 2, 4, 2, 0})
+assertEq(get_indents(), {0, 2, 2, 4, 4, 4, 4, 0, 4, 4, 2, 4, 2, 0})
 -- Try indenting each line individually to check it doesn't move (ie doesn't
 -- give a different result from reindenting the whole thing)
 local text = buffer:get_text()
