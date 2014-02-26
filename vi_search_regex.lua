0a0cae116fad17968fa1cc0428efea1241555c20
Chris Emerson <github@mail.nosreme.org>
2014-02-26 23:18:44 +0000

    Implement wrapping search.

--- vi_search_regex.lua
+++ vi_search_regex.lua
@@ -2,21 +2,22 @@ local M = {}
 
 local vi_regex = require'vi_regex'
 
--- Replace textadept's events.FIND handler with one implementing better regex.
-
-function M.install()
-    events.connect(events.FIND, M.find, 1)
-end
-
 M.search_hl_indic = _SCINTILLA.next_indic_number()
 
+-- Set up our indicator style.
 local function set_colours()
-    buffer.indic_fore[M.search_hl_indic] = 0x00FFFF
+    buffer.indic_fore[M.search_hl_indic] = 0x0000FF
     buffer.indic_style[M.search_hl_indic] = _SCINTILLA.constants.INDIC_ROUNDBOX
     buffer.indic_alpha[M.search_hl_indic] = 100
     -- Find all occurrences to highlight.
     buffer.indicator_current = M.search_hl_indic
     buffer:indicator_clear_range(0, buffer.length)
+end
+
+-- Replace textadept's events.FIND handler with one implementing better regex.
+
+function M.install()
+    events.connect(events.FIND, M.find, 1)
 end
 
 -- Find expression forwards from the current point.
@@ -26,16 +27,22 @@ function M.find(regex, forward)
     local startpos = buffer.current_pos + 1
     local endpos = buffer.length
     
-    local text = buffer:text_range(startpos, endpos)
+    local function search(startpos, endpos)
+        local m = pat:match(buffer:text_range(startpos, endpos))
+        if m then
+            -- Adjust result to take account of startpos
+            m._start = m._start + startpos - 1
+            m._end = m._end + startpos
+        end
+        return m
+    end
     
-    local m = pat:match(text)
+    local m = search(startpos, endpos) or search(0, endpos)
     
-    if m and m._start and m._end then
-        local s, e = m._start, m._end
-        s = s + startpos - 1
-        e = e + startpos
-        --ui.print(s, e)
+    if m then
         set_colours()
+        local s, e = m._start, m._end
+        --ui.print(s, e)
         buffer:indicator_fill_range(s, e-s)
         buffer:goto_pos(s)
     else
