ee325bb7e26911b3be50c178ad114012ada999af
Chris Emerson <github@mail.nosreme.org>
2014-02-27 22:42:24 +0000

    Fix to module names.

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Copyright (C) 2014 Chris Emerson <github@mail.nosreme.org>
 -- See LICENSE for details (MIT license).
 
-local M = require 'regex_find'
+local M = require 'ta-regex.regex_find'
 
 return M
