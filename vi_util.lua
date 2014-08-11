a0c715e00f546334ffa4cdfa563e2e243ad6b751
Chris Emerson <github@mail.nosreme.org>
2014-08-11 18:06:02 +0100

    Slight tweak to pretty-tostring: use __tostring if present on a table.

--- vi_util.lua
+++ vi_util.lua
@@ -1,11 +1,24 @@
 -- Lua utility functions
 local M = {}
 
+-- Save the original tostring (in case a user replaces the original with
+-- this one).
+local tostring = tostring
+
 -- Pretty-print Lua values.
 function M.tostring(a)
     if type(a) == "string" then
         return '"' .. a .. '"'
-    elseif type(a) ~= 'table' then return tostring(a) end
+    elseif type(a) ~= 'table' then
+        return tostring(a)
+    else
+        local mt = getmetatable(a)
+        if mt and mt.__tostring then
+            return tostring(a)
+        end
+    end
+    
+    
     local maxn = 0
     local sbits = {'{'}
     for i,v in ipairs(a) do
