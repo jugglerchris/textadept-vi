7e2b9cbdfa4d746c002ab77439660e1c2216b02b
Chris Emerson <github@mail.nosreme.org>
2015-01-22 21:02:11 +0000

    Add simple test suite and regex tests borrowed from textadept-vi.

---
+++ test/test.lua
@@ -0,0 +1,78 @@
+-- Test utilities
+local M = {}
+
+-- Get access to the regex modules
+package.path = "../?.lua;" .. package.path
+
+local eq
+local tableEq
+
+function eq(a, b)
+    -- Easy case: builtin equal works for most cases.
+    if a == b then return true end
+    
+    if type(a) ~= 'table' or type(b) ~= 'table' then
+        -- If not both tables, then not equal.
+        return false
+    end
+    return tableEq(a, b)
+end
+
+-- Compare two tables, treating them as the same if they key pairs
+-- are equal.
+function tableEq(a, b)
+  -- First, check that every key in a matches one in b.
+  for k,v in pairs(a) do
+      if not eq(v, b[k]) then return false end
+  end
+  
+  -- Second, check that every key in b exists in a.
+  -- We don't need to compare - if the key is in a then we've already
+  -- checked.
+  for k,_ in pairs(b) do
+    if a[k] == nil then return false end
+  end
+  
+  -- They must be equal
+  return true
+end
+
+-- Pretty-print tables
+function M.tostring(a)
+    if type(a) == "string" then
+        return '"' .. a .. '"'
+    elseif type(a) ~= 'table' then return tostring(a) end
+    local maxn = 0
+    local sbits = {'{'}
+    for i,v in ipairs(a) do
+        table.insert(sbits, M.tostring(v) .. ", ")
+        maxn = i
+    end
+    for k,v in pairs(a) do
+        -- Do the non-contiguous-integer keys
+        if type(k) == 'number' and k == math.ceil(k) and k <= maxn and k >= 1 then 
+           -- Ignore an integer key we've already seen
+        else
+            table.insert(sbits, '['..M.tostring(k)..'] = '..M.tostring(v)..', ')
+        end
+    end
+    table.insert(sbits, '}')
+    return table.concat(sbits)
+end
+
+--- Assert that a and b are equal.  Tables are equal if their keys
+--  and values are equal.  Calls error() with level to report an error.
+local function assertEqLevel(a, b, level)
+    if not eq(a,b) then
+        error("Failed assertion: [["..M.tostring(a).."]] != [["..M.tostring(b).."]]\n", level)
+    end
+end
+
+--- Assert that a and b are equal.  Tables are equal if their keys
+--  and values are equal.  Returns true or calls error().  
+function M.assertEq(a, b)
+    return assertEqLevel(a, b, 2)
+end
+
+
+return M
---
+++ test/test_regex2.lua
@@ -0,0 +1,52 @@
+local test = require'test'
+local assertEq = test.assertEq
+local function log(x) test.log(tostring(x) .. "\n") end
+local regex = require('regex')
+local compile = regex.compile
+
+local pat
+-- Check \s, \S, \w, \W, \d, \D
+pat = compile('x\\s')
+
+assertEq(pat:match("xxx j"), {_start=3,_end=4})
+assertEq(pat:match("xxx\tj"), {_start=3,_end=4})
+
+pat = compile('\\w\\s')
+assertEq(pat:match("x j"), {_start=1,_end=2})
+assertEq(pat:match("Z_ j"), {_start=2, _end=3})
+assertEq(pat:match("Z; j"), nil)
+
+pat = compile('\\W\\w')
+assertEq(pat:match(";2 177_x j"), {_start=1,_end=2})
+
+pat = compile('\\S\\s\\S')
+assertEq(pat:match(";2 177_x j"), {_start=2,_end=4})
+
+pat = compile('\\d+')
+assertEq(pat:match("askdfj1317 jlj"), {_start=7, _end=10})
+
+pat = compile('\\D+')
+assertEq(pat:match("1230 _abc;_ 8"), {_start=5, _end=12})
+
+-- Check C-style escapes
+pat = compile('\\t+')
+assertEq(pat:match("abc\t\t foo"), {_start=4, _end=5})
+
+pat = compile('\\n+')
+assertEq(pat:match("abc\n\n foo"), {_start=4, _end=5})
+
+pat = compile('\\r+')
+assertEq(pat:match("abc\r\r foo"), {_start=4, _end=5})
+
+pat = compile('\\f+')
+assertEq(pat:match("abc\f\f foo"), {_start=4, _end=5})
+
+pat = compile('\\a+')
+assertEq(pat:match("abc\a\a foo"), {_start=4, _end=5})
+
+pat = compile('\\e+')
+assertEq(pat:match("abc\027\027 foo"), {_start=4, _end=5})
+
+-- Check them inside charsets
+pat = compile('[ \\t]+')
+assertEq(pat:match("abc\t\t tfoo"), {_start=4, _end=6})
