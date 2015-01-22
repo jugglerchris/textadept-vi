38ff07f0307340517f04de5289a9751f90c3a2c4
Chris Emerson <github@mail.nosreme.org>
2015-01-22 22:01:59 +0000

    Add test for issue #3.

--- test/test_bugs.lua
+++ test/test_bugs.lua
@@ -9,3 +9,26 @@ assertEq(pat:match("foobar"), {_start=1,_end=0})
 
 pat = compile('()|')
 assertEq(pat:match("foobar"), {_start=1,_end=0, groups={{1,0}},})
+
+pat = compile("((a)|(ab))((c)|(bc))")
+assertEq(pat:match("ac"), {_start=1,_end=2, groups={
+                                               {1,1},
+                                               {1,1},
+                                               nil,
+                                               {2,2},
+                                               {2,2},
+                                               nil}})
+assertEq(pat:match("abc"), {_start=1,_end=3, groups={
+                                               {1,1},
+                                               {1,1},
+                                               nil,
+                                               {2,3},
+                                               nil,
+                                               {2,3}}})
+assertEq(pat:match("abbc"), {_start=1,_end=4, groups={
+                                               {1,2},
+                                               nil,
+                                               {1,2},
+                                               {3,4},
+                                               nil,
+                                               {3,4}}})
