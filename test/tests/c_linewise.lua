975bc18e978e9904e2c1176744d1edac9f5dbe71
Chris Emerson <github@mail.nosreme.org>
2015-06-26 23:12:20 +0100

    Add test of 'c' with linewise movement (fails).

---
+++ test/tests/c_linewise.lua
@@ -0,0 +1,10 @@
+test.open('words.txt')
+local lineno = test.lineno
+local colno = test.colno
+local assertEq = test.assertEq
+
+test.key('c', 'j', 'x', 'y', 'z', 'escape')
+assertEq(lineno(), 0)
+assertEq(colno(), 2)
+assertEq(buffer:get_line(0), 'xyz\n')
+assertEq(buffer:get_line(1), 'some miscellaneous text\n')
