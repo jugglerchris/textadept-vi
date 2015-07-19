5d8a33b19efcb887661f8a9c1fe25c2006326ef6
Chris Emerson <github@mail.nosreme.org>
2015-07-19 17:59:37 +0100

    Fix paragraph motions to be exclusive, and fix the test.

--- test/tests/curly_brace.lua
+++ test/tests/curly_brace.lua
@@ -1,6 +1,8 @@
 -- Test the { and } motions
 test.open('wrap.txt')
 
+local vi_ta_util = require 'vi_ta_util'
+
 local assertEq = test.assertEq
 local lineno = test.lineno
 local line_length = vi_ta_util.line_length
@@ -24,11 +26,11 @@ test.key('d', '2', '{')
 assertEq(lineno(), 0)
 assertEq(line_length(lineno()), 0)
 assertEq(buffer.line_count, 5)
-assertEq(state.registers['"'],[[
+assertEq(state.registers['"'],{line=true, text=[[
 this
 is
 a
 paragraph
 
 this is a paragraph with a long line.  There are sentence breaks inside the line, and it goes on and on and on.  For at least three lines.  This should be wrapped to separate lines.
-]])
+]]})
