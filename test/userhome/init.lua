2f0b23c49bc95b3fb7c955cf256055abe9bf5e4e
Chris Emerson <github@mail.nosreme.org>
2018-05-14 22:44:06 +0100

    Textadept 10 update: move the contents of properties.lua into init.lua.

--- test/userhome/init.lua
+++ test/userhome/init.lua
@@ -1,5 +1,14 @@
 -- Turn on coverage analysis if available
 pcall(require, 'luacov')
+
+-- Used to be in properties.lua
+buffer.margin_width_n[0] = 0
+buffer.margin_width_n[1] = 0
+buffer.margin_width_n[2] = 1
+buffer.margin_width_n[3] = 1
+buffer.use_tabs = false
+buffer.indent = 2
+
 ok, msg = xpcall(function()
 -- Make textadept-vi available
 package.path = _USERHOME .. "/../../?.lua;".._USERHOME .. "/../scripts/?.lua;" .. package.path
