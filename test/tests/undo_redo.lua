4f13f2137961f89d6841109439194e80e085995c
Chris Emerson <github@mail.nosreme.org>
2020-05-20 07:51:30 +0100

    Down to 3 failing tests.

--- test/tests/undo_redo.lua
+++ test/tests/undo_redo.lua
@@ -4,8 +4,8 @@ local colno = test.colno
 local assertEq = test.assertEq
 
 test.keys('dd')
-assertEq(buffer:get_line(0), 'hey bee cee dee ee eff\n')
+assertEq(buffer:get_line(1), 'hey bee cee dee ee eff\n')
 test.keys('u')
-assertEq(buffer:get_line(0), 'one two three four five\n')
+assertEq(buffer:get_line(1), 'one two three four five\n')
 test.key('c-r')
-assertEq(buffer:get_line(0), 'hey bee cee dee ee eff\n')
+assertEq(buffer:get_line(1), 'hey bee cee dee ee eff\n')
